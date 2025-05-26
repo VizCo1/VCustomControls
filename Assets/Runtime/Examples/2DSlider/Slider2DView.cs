0ba45e3518231bdcbfe0a515ecf5afbac947acf5
Víctor Izquierdo <62243456+VizCo1@users.noreply.github.com>
2025-05-26 23:58:58 +0200

    . (#2)

---
+++ Assets/Runtime/Examples/2DSlider/Slider2DView.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace VCustomComponents.Runtime.Examples._2DSlider
+{
+    [RequireComponent(typeof(UIDocument))]
+    public class Slider2DView : MonoBehaviour
+    {
+        private UIDocument _document;
+
+        private void Awake()
+        {
+            _document = GetComponent<UIDocument>();
+        }
+
+        private void Start()
+        {
+            var slider2D = _document.rootVisualElement.Q<VCustomComponents.Runtime.CustomComponents.Slider2D>();
+
+            slider2D.RegisterValueChangedCallback(OnSlider2DValueChanged);
+        }
+
+        private void OnSlider2DValueChanged(ChangeEvent<Vector2> evt)
+        {
+            Debug.Log(evt.newValue);
+        }
+    }
+}
