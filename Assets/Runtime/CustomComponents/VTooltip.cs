9cb81c4d8958c0bbd0153e9418c70034aca8f2c4
Víctor <62243456+VizCo1@users.noreply.github.com>
2025-06-27 20:03:17 +0200

    .

--- Assets/Runtime/CustomComponents/VTooltip.cs
+++ Assets/Runtime/CustomComponents/VTooltip.cs
@@ -8,40 +8,27 @@ namespace VCustomComponents
     public partial class VTooltip : Label
     {
         private const bool DoOneTime = true;
-        
+
+        public static readonly string TooltipClass = "tooltip";
+
         [Header(nameof(VTooltip))]
-        
-        [UxmlAttribute]
-        private int Example { get; set; }
+        [UxmlAttribute, Tooltip("Fade duration in seconds, if set to -1 there will be no fade")]
+        private float FadeTimeInSeconds { get; set; } = -1f;
 
         public VTooltip() 
         {
-            pickingMode = PickingMode.Ignore;
             style.position = Position.Absolute;
-            
+            AddToClassList(TooltipClass);
 #if UNITY_EDITOR
             if (!Application.isPlaying)
                 return;
 #endif
-            this.SetDisplay(false);
-            
-            RegisterCallbackOnce<AttachToPanelEvent>(OnAttachedToPanel);
-            RegisterCallbackOnce<GeometryChangedEvent>(OnGeometryChanged);
-        }
-    
-        private void OnAttachedToPanel(AttachToPanelEvent evt)
-        {
-        
-        }
-    
-        private void OnGeometryChanged(GeometryChangedEvent evt)
-        {
-        
+            style.opacity = 0;
         }
 
         public void Show(VisualElement target, VTooltipPosition tooltipPosition)
         {
-            this.SetDisplay(true);
+            FadeIn();
             
             text = target.tooltip;
 
@@ -58,7 +45,7 @@ namespace VCustomComponents
         
         public void Hide()
         {
-            this.SetDisplay(false);
+            FadeOut();
         }
         
         private Vector2 GetTooltipPosition(VisualElement target, VTooltipPosition tooltipPosition)
@@ -71,6 +58,30 @@ namespace VCustomComponents
                 VTooltipPosition.Left => new Vector2(target.worldBound.xMin - resolvedStyle.width, target.worldBound.center.y - resolvedStyle.height * 0.5f),
                 _ => throw new ArgumentOutOfRangeException(nameof(tooltipPosition), tooltipPosition, null)
             };
+        }
+
+        private void FadeIn()
+        {
+            style.opacity = 0;
+            
+            schedule
+                .Execute(() =>
+                { 
+                    style.opacity = resolvedStyle.opacity + 0.05f;  
+                })
+                .Until(() => resolvedStyle.opacity >= 1f);
+        }
+
+        private void FadeOut()
+        {
+            style.opacity = 1;
+            
+            schedule
+                .Execute(() =>
+                {
+                    style.opacity = resolvedStyle.opacity - 0.05f; 
+                })
+                .Until(() => resolvedStyle.opacity <= 0f);
         }
     }
 }
