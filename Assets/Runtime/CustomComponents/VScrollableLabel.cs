fa618910ff86ae5dfa5e79e7da85627ba34e2ad1
Víctor <62243456+VizCo1@users.noreply.github.com>
2025-06-30 22:04:35 +0200

    .

--- Assets/Runtime/CustomComponents/VScrollableLabel.cs
+++ Assets/Runtime/CustomComponents/VScrollableLabel.cs
@@ -4,15 +4,23 @@ using UnityEngine.UIElements;
 namespace VCustomComponents
 {
     [UxmlElement]
-    public partial class VScrollableLabel : Label
+    public partial class VScrollableLabel : VisualElement
     {
+        public static readonly string ScrollableLabelClass = "scrollable-label";
+        public static readonly string ScrollableLabelContainerClass = ScrollableLabelClass + "-container";
+        
         [Header(nameof(VScrollableLabel))]
         
-        [UxmlAttribute]
-        private int Example { get; set; }
+        private Label Label { get; set; }
 
         public VScrollableLabel() 
         {
+            AddToClassList(ScrollableLabelContainerClass);
+            
+            Label = new Label("Label");
+            Label.AddToClassList(ScrollableLabelClass);
+            Add(Label);
+            
             RegisterCallbackOnce<AttachToPanelEvent>(OnAttachedToPanel);
             RegisterCallbackOnce<GeometryChangedEvent>(OnGeometryChanged);
         }
