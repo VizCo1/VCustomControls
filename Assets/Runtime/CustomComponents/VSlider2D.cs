57dd57421f877233b176c2adad51b775eaae22ea
Víctor Izquierdo <62243456+VizCo1@users.noreply.github.com>
2025-07-08 14:37:47 +0200

    . (#13)

--- Assets/Runtime/CustomComponents/VSlider2D.cs
+++ Assets/Runtime/CustomComponents/VSlider2D.cs
@@ -7,12 +7,13 @@ namespace VCustomComponents
     [UxmlElement]
     public partial class VSlider2D : VisualElement, INotifyValueChanged<Vector2>
     {
-        private static readonly BindingId ValueProperty = (BindingId) nameof(value);
-    
         public static readonly string VSlider2DClass = "slider2d"; 
         public static readonly string DraggerClass = VSlider2DClass + "-dragger-element";
-    
         public static readonly string DraggerName = "DraggerElement";
+    
+        private static readonly BindingId ValueProperty = (BindingId) nameof(value);
+        
+        private readonly VisualElement _draggerElement;
     
         [Header(nameof(VSlider2D))]
         
@@ -24,7 +25,7 @@ namespace VCustomComponents
             {
                 _minValue = value;
             
-                SetValueWithoutNotify(this.value);
+                SetValueWithoutNotify(_value);
             }
         }
 
@@ -36,7 +37,7 @@ namespace VCustomComponents
             {
                 _maxValue = value;
             
-                SetValueWithoutNotify(this.value);
+                SetValueWithoutNotify(_value);
             }
         }
     
@@ -70,49 +71,74 @@ namespace VCustomComponents
         private Vector2 _value;
         private Vector2 _minValue = Vector2.zero;
         private Vector2 _maxValue = Vector2.one;
-    
-        private VisualElement _draggerElement;
-
+        
         public VSlider2D()
         {
             AddToClassList(VSlider2DClass);
         
-            CreateDraggerElement();
+            _draggerElement = new VisualElement();
+            _draggerElement.AddToClassList(DraggerClass);
+            _draggerElement.name = DraggerName;
+            _draggerElement.usageHints = UsageHints.DynamicTransform;
+            
+            Add(_draggerElement);
 
             RegisterCallbackOnce<GeometryChangedEvent>(OnGeometryChanged);
-        
-            RegisterCallback<MouseDownEvent>(OnMouseDown);
-            RegisterCallback<MouseMoveEvent>(OnMouseMove);
-            RegisterCallback<MouseUpEvent>(OnMouseUp);
         }
 
         private void OnGeometryChanged(GeometryChangedEvent evt)
         {
-            _offset = new Vector2(_draggerElement.resolvedStyle.width / 2f, _draggerElement.resolvedStyle.height / 2f);
+            _offset = new Vector2(_draggerElement.resolvedStyle.width * 0.5f, _draggerElement.resolvedStyle.height * 0.5f);
+        
+            MoveDragger();
+        }
+        
+        protected override void HandleEventBubbleUp(EventBase evt)
+        {
+            if (evt.eventTypeId == PointerMoveEvent.TypeId())
+            {
+                OnMouseMove((PointerMoveEvent)evt);
+            }
+            else if (evt.eventTypeId == PointerDownEvent.TypeId())
+            {
+                OnMouseDown((PointerDownEvent)evt);
+            }
+            else if (evt.eventTypeId == PointerUpEvent.TypeId())
+            {
+                OnMouseUp((PointerUpEvent)evt);
+            }
+        }
+        
+        public void SetValueWithoutNotify(Vector2 newValue)
+        {
+            var validX = Mathf.Clamp(newValue.x, _minValue.x, _maxValue.x);
+            var validY = Mathf.Clamp(newValue.y, _minValue.y, _maxValue.y);
+            
+            _value = new Vector2(validX, validY);
         
             MoveDragger();
         }
 
-        private void OnMouseDown(MouseDownEvent evt)
+        private void OnMouseDown(PointerDownEvent evt)
         {
             if (evt.button != 0)
                 return;
         
-            value = RemapBetweenMinAndHighValues(evt.localMousePosition);
+            value = RemapBetweenMinAndHighValues(evt.localPosition);
         
             _canMove = true;
             this.CapturePointer(evt.button);
         }
     
-        private void OnMouseMove(MouseMoveEvent evt)
+        private void OnMouseMove(PointerMoveEvent evt)
         {
             if (!_canMove)
                 return;
         
-            value = RemapBetweenMinAndHighValues(evt.localMousePosition);
+            value = RemapBetweenMinAndHighValues(evt.localPosition);
         }
     
-        private void OnMouseUp(MouseUpEvent evt)
+        private void OnMouseUp(PointerUpEvent evt)
         {
             if (evt.button != 0)
                 return;
@@ -129,6 +155,18 @@ namespace VCustomComponents
             var adjustedPosX = remappedPercentageX * resolvedStyle.width - _offset.x - resolvedStyle.paddingLeft - resolvedStyle.borderLeftWidth;
             var adjustedPosY = remappedPercentageY * resolvedStyle.height - _offset.y - resolvedStyle.paddingTop - resolvedStyle.borderTopWidth;
             
+            adjustedPosX = Mathf.Clamp(
+                adjustedPosX, 
+                resolvedStyle.borderLeftWidth - resolvedStyle.borderRightWidth, 
+                resolvedStyle.width - _draggerElement.resolvedStyle.width 
+                                    - resolvedStyle.borderRightWidth - resolvedStyle.borderLeftWidth);
+            
+            adjustedPosY = Mathf.Clamp(
+                adjustedPosY, 
+                resolvedStyle.borderTopWidth - resolvedStyle.borderBottomWidth, 
+                resolvedStyle.height - _draggerElement.resolvedStyle.height 
+                                     - resolvedStyle.borderBottomWidth - resolvedStyle.borderTopWidth);
+            
             _draggerElement.style.translate = new Translate(new Length(adjustedPosX, LengthUnit.Pixel), new Length(adjustedPosY, LengthUnit.Pixel));
         }
     
@@ -138,25 +176,6 @@ namespace VCustomComponents
             var posY = _minValue.y + position.y / resolvedStyle.height * (_maxValue.y - _minValue.y);
         
             return new Vector2(posX, posY);
-        }
-    
-        private void CreateDraggerElement()
-        {
-            _draggerElement = new VisualElement();
-            _draggerElement.AddToClassList(DraggerClass);
-            _draggerElement.name = DraggerName;
-            _draggerElement.usageHints = UsageHints.DynamicTransform;
-            Add(_draggerElement);
-        }
-
-        public void SetValueWithoutNotify(Vector2 newValue)
-        {
-            var validX = Mathf.Clamp(newValue.x, _minValue.x, _maxValue.x);
-            var validY = Mathf.Clamp(newValue.y, _minValue.y, _maxValue.y);
-            
-            _value = new Vector2(validX, validY);
-        
-            MoveDragger();
         }
     }
 }
